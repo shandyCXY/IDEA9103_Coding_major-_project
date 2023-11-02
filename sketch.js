f2295a193008a1e7c538cc676c161151bd065685
shandyCXY <xche0522@uni.sydney.edu.au>
2023-11-03 10:56:48 +1100

    color input from groupmate

--- sketch.js
+++ sketch.js
@@ -1,11 +1,58 @@
-let inc = 0.1; //each segment vector change
+let inc = 0.1;
 let scl = 10; //segmet size
 let cols, rows;
+let skyColorsFrom = [];
+let skyColorsTo = [];
+let skyColorsLerpA = [];
+let skyColorsLerpB = [];
+let skyColorsLerpC = [];
+let skyColorsLerpD = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cols = floor(windowWidth / scl);
   rows = floor(windowHeight / scl);
+  //Define the color arrays for lerpColor().
+
+  //The colors are: [0]navy blue, [1]sea green, [2]bright yellow, [3]orange red, [4]dark red
+  skyColorsFrom.push(
+    color(62, 84, 143),
+    color(125, 155, 147),
+    color(255, 214, 101),
+    color(193, 113, 67),
+    color(205, 74, 74)
+  );
+
+  //The colors are: [0]sea green, [1]bright yellow, [2]orange red
+  skyColorsTo.push(
+    color(125, 155, 147),
+    color(255, 214, 101),
+    color(193, 113, 67),
+    color(205, 74, 74)
+  );
+
+  //Build four arrays: skyColorLerp A/B/C/D to contain the lerpColor() results between the
+  //skyColorsFrom[] and skyColorsTo[]
+
+  //A
+  for (let k = 1; k < 9; k++) {
+    skyColorsLerpA.push(lerpColor(skyColorsFrom[0], skyColorsTo[0], k* 0.11));
+  }
+
+  //B
+  for (let k = 1; k < 9; k++) {
+    skyColorsLerpB.push(lerpColor(skyColorsFrom[1], skyColorsTo[1], k * 0.11));
+  }
+
+  //C
+  for (let k = 1; k < 9; k++) {
+    skyColorsLerpC.push(lerpColor(skyColorsFrom[2], skyColorsTo[2], k * 0.11));
+  }
+
+  //D
+  for (let k = 1; k < 9; k++) {
+    skyColorsLerpD.push(lerpColor(skyColorsFrom[3], skyColorsTo[3], k* 0.11));
+  }
 }
 
 function draw() {
@@ -14,11 +61,11 @@ function draw() {
   translate(0, windowHeight / 2);
   let yoff = 0;
   for (let y = 0; y < rows / 2; y++) {
-    let xoff = 0;
+    let xoff= 0;
     for (let x = 0; x < cols; x++) {
       let index = (x + y * width) * 3;
       let angle = noise(xoff, yoff) * TWO_PI;
-      let v = p5.Vector.fromAngle(angle *(-0.2));
+      let v = p5.Vector.fromAngle(angle * -0.2);
       xoff += inc;
       //rect(x*scl,y*scl,scl,scl);
       noStroke();
@@ -29,16 +76,22 @@ function draw() {
       rect(0, 0, 23, 4);
       pop();
     }
-    colorMode(HSB);
+
+    if (y < 9) {
+      fill(skyColorsLerpA[round(y)]);
+      yoff += inc;
+    } else if (9 < y < 18) {
+      fill(skyColorsLerpB[round(y) % 9]);
+      yoff += inc;
+    } else if (18 < y < 27) {
+      fill(skyColorsLerpC[round(y) % 9]);
+      yoff += inc;
+    } else {
+      fill(skyColorsLerpD[round(y) % 9]);
+      yoff += inc;
     
-    if (y <30) {
-      fill(255-2*y,y+234,y+23*y);
-      
-    } 
-    else{
-      fill(134+2*y,12+4*y, 344- y,300);
     }
-    yoff += inc;
+    console.log(skyColorsLerpA[round(y)]);
   }
 
   //reference web:https://www.youtube.com/watch?v=BjoM9oKOAKY&t=3s.
